1a93f6fc5b5c888f20c317c1f86d79f824ab3819
Douglas Packard <dpackar2@illinois.edu>
2022-11-05 17:33:45 -0500

    finalized sql files

--- EmployeeSQL/create_tables.sql
+++ EmployeeSQL/create_tables.sql
@@ -10,11 +10,6 @@ CREATE TABLE titles (
   title STRING
 );
 
--- SHOW DATESTYLE;
--- SET DATESTYLE TO SQL, MDY;
-
--- DROP TABLE employees;
-
 CREATE TABLE employees (
   emp_no INT PRIMARY KEY,
   emp_title_id VARCHAR(5) NOT NULL,
@@ -32,8 +27,6 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
--- DROP TABLE dept_manager;
-
 CREATE TABLE dept_manager (
   dept_no VARCHAR(5),
   emp_no INT,
@@ -41,19 +34,9 @@ CREATE TABLE dept_manager (
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
--- DROP TABLE dept_emp;
-
 CREATE TABLE dept_emp (
   emp_no INT,
   dept_no VARCHAR(5),
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
-
-SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-FROM employees AS e
-INNER JOIN salaries AS s
-    ON (e.emp_no=s.emp_no);
-    
-SELECT first_name, last_name, hire_date
-FROM employees 
